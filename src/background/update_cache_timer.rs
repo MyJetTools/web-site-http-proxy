38a6b4c75b250d4d50bc4ba520de229c29e02a28
amigin <amigin@gmail.com>
2023-07-03 22:13:23 +0300

    Made auto load content

--- src/background/update_cache_timer.rs
+++ src/background/update_cache_timer.rs
@@ -1,8 +1,8 @@
 use std::sync::Arc;
 
-use rust_extensions::MyTimerTick;
+use rust_extensions::{date_time::DateTimeAsMicroseconds, MyTimerTick};
 
-use crate::app::AppContext;
+use crate::{app::AppContext, operations};
 
 pub struct UpdateCacheTimer {
     app: Arc<AppContext>,
@@ -17,8 +17,20 @@ impl UpdateCacheTimer {
 #[async_trait::async_trait]
 impl MyTimerTick for UpdateCacheTimer {
     async fn tick(&self) {
+        let started = DateTimeAsMicroseconds::now();
         while let Some(path) = self.app.cached_pages.get_expired_cache_item().await {
-            self.app.cached_pages.remove(&path).await;
+            let now = DateTimeAsMicroseconds::now();
+
+            if now.duration_since(started).as_positive_or_zero().as_secs() > 30 {
+                break;
+            }
+
+            match operations::load_from_remote_url(&self.app, path.as_str()).await {
+                Ok(_) => {}
+                Err(_) => {
+                    self.app.cached_pages.remove(&path).await;
+                }
+            }
         }
     }
 }
